93016d7c5bed274be20452392f2037ae711c1bcb
Ben Woodcroft <donttrustben@gmail.com>
2015-04-18 11:00:38 +1000

    first draft version that passes tests

--- tests/test_tree_sisters.py
+++ tests/test_tree_sisters.py
@@ -2,30 +2,63 @@ from tree_sisters.tree_annotator import TreeAnnotator
 from nose.tools import assert_equals, assert_true
 from skbio.tree import TreeNode
 from StringIO import StringIO
-import IPython
 
 class TestTree2TaxNamedClusters:
     def testFindParents(self):
         ann = TreeAnnotator()
         
         tree = TreeNode.read(StringIO("(((A:1, B:2)'g__genus1':3, (C:4, D:5)'g__genus2':6)'f__family':10)root;"))
-        assert_equals('B', ann.find_parent_name(tree, tree.find('B')), 'parent is self')
+        assert_equals('g__genus1', ann.find_named_parent(tree, tree.find('B')).name, 'self is named')
         
         tree = TreeNode.read(StringIO("(((A:1, 2475:2)'g__genus1':3, (C:4, D:5)'g__genus2':6)'f__family':10)root;"))
-        assert_equals('g__genus1', ann.find_parent_name(tree, tree.find('2475')), 'parent directly above')
+        assert_equals('g__genus1', ann.find_named_parent(tree, tree.find('2475')).name, 'parent directly above')
         
         tree = TreeNode.read(StringIO("(((A:1, 2475:2):3, (C:4, D:5)'g__genus2':6)'f__family':10)root;"))
-        assert_equals('f__family', ann.find_parent_name(tree, tree.find('2475')), 'parent 2 above')
+        assert_equals('f__family', ann.find_named_parent(tree, tree.find('2475')).name, 'parent 2 above')
         
         tree = TreeNode.read(StringIO("(((A:1, 2475:2):3, (C:4, D:5)'g__genus2':6)'f__family':10);"))
-        assert_equals(None, ann.find_parent_name(tree, tree.find('f__family').parent), 'parent of root')
+        assert_equals(None, ann.find_named_parent(tree, tree.find('f__family').parent), 'parent of root')
         
         tree = TreeNode.read(StringIO("(((A:1, 2475:2):3, (C:4, D:5)'g__genus2':6):10);"))
-        assert_equals(None, ann.find_parent_name(tree, tree.find('g__genus2').parent), 'no parent before root')
+        assert_equals(None, ann.find_named_parent(tree, tree.find('g__genus2').parent), 'no parent before root')
         
-    def testSistersSelf(self):
+              
+    def testSistersHelloWorld(self):
+        ann = TreeAnnotator()
+        tree = TreeNode.read(StringIO("(((A:1, B:2):3, (C:4, D:5)'g__genus2':6)'f__family':10)root;"))
+        sisters = ann.find_sisters(tree, tree.find('B'))
+        assert_equals(['g__genus2'], [s.name for s in sisters])   
+        
+    def testSistersSelfNoParent(self):
         ann = TreeAnnotator()
         tree = TreeNode.read(StringIO("(((A:1, B:2)'g__genus1':3, (C:4, D:5)'g__genus2':6)'f__family':10)root;"))
         sisters = ann.find_sisters(tree, tree.find('B'))
-        assert_equals(['B'], [s.name for s in sisters])
+        assert_equals([], [s.name for s in sisters])  
+
+    def testSistersTwoSistersEqualLevel(self):
+        ann = TreeAnnotator()
+        tree = TreeNode.read(StringIO("((A:1, B:2):3, ((C:1,D:1)'g2':1, (E:1,F:5)'g3':6):10)root;"))
+        print(tree.ascii_art())
+        sisters = ann.find_sisters(tree, tree.find('B'))
+        assert_equals(sorted(['g2','g3']), sorted([s.name for s in sisters]))
+
+    def testSistersOneIncompleteSister(self):
+        ann = TreeAnnotator()
+        tree = TreeNode.read(StringIO("((A:1, B:2):3, ((C:1,D:1):1, (E:1,F:5)'g3':6):10)root;"))
+        print(tree.ascii_art())
+        sisters = ann.find_sisters(tree, tree.find('B'))
+        assert_equals(sorted(['g3']), sorted([s.name for s in sisters]))
+        
+    def testSistersSisterWithDescendentNames(self):
+        ann = TreeAnnotator()
+        tree = TreeNode.read(StringIO("((A:1, B:2):3, (((a:1,b:1)'s1':1,D:1)'g2':1, (E:1,F:5)'g3':6):10)root;"))
+        print(tree.ascii_art())
+        sisters = ann.find_sisters(tree, tree.find('B'))
+        assert_equals(sorted(['g2','g3']), sorted([s.name for s in sisters]))
+        
+    def testFullTaxonomy(self):
+        ann = TreeAnnotator()
+        tree = TreeNode.read(StringIO("(((A:1, B:2):3, (C:4, D:5)'g__genus2':6)'f__family':10)root;"))
+        assert_equals('f__family; g__genus2', ann.full_taxonomy(tree, tree.find('D')))
+
         
--- tree_sisters/tree_annotator.py
+++ tree_sisters/tree_annotator.py
@@ -1,12 +1,26 @@
 from tree2tax.tree2tax import TaxonomyFunctions
 
 class TreeAnnotator:
-    def find_parent_name(self, tree, node):
-        n = node
-        while (n is not None):
+    def full_taxonomy(self, tree, node, separator='; '):
+        name = ''
+        if node.is_tip():
+            node = node.parent
+        while node.parent:
+            current_name = TaxonomyFunctions().taxonomy_from_node_name(node.name)
+            if current_name:
+                if name == '':
+                    name = current_name
+                else:
+                    name = "%s%s%s" % (current_name, separator, name)
+            node = node.parent
+        return name
+        
+    def find_named_parent(self, tree, node):
+        n = node.parent
+        while n is not None:
             name = TaxonomyFunctions().taxonomy_from_node_name(n.name)
-            if (name is not None):
-                return name
+            if name is not None:
+                return n
             else:
                 n = n.parent
         return None
@@ -17,20 +31,41 @@ class TreeAnnotator:
         stack = Stack() # collection of nodes that may contain sister lineages
         sisters = []
         n = node
+        if n is None: return sisters
+        visited_nodes = set([n])
+        limited_by_parent = False
+        
         while len(sisters) == 0 and n is not None:
-            while n is not None:
-                name = TaxonomyFunctions().taxonomy_from_node_name(n.name)
-                if name is not None:
-                    # this node is a sister
-                    sisters.append(n)
-                else:
-                    # not named, descend further
-                    for d in n.children:
-                        stack.push(d)
-
-            if len(sisters) == 0:
+            try:
+                parent = n
+                while True:
+                    name = TaxonomyFunctions().taxonomy_from_node_name(n.name)
+                    if name is not None and parent != n and not n.is_tip():
+                        # this node is a sister
+                        sisters.append(n)
+                    else:
+                        # not named, descend further
+                        for d in n.children:
+                            if d not in visited_nodes:
+                                visited_nodes.add(d)
+                                stack.push(d)
+                    n = stack.pop()
+            except IndexError: #stack is empty
+                if limited_by_parent:
+                    break
                 # no sisters found at this level, go up one and try again
                 n = n.parent
+                visited_nodes.add(n)
+                # if the new parent node is named, then we've reached the limit,
+                # don't go any further
+                if TaxonomyFunctions().taxonomy_from_node_name(n.name):
+                    limited_by_parent = True
+              
+        if len(sisters) == 1 and sisters[0] == self.find_named_parent(tree, node):
+            # Don't return a parent as a sister
+            return []
+        else: 
+            return sisters
             
 class Stack:
     def __init__(self):
